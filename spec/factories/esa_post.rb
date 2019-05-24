ef0a4a4e5353838feb3aae02abbff551b9ce2c4e
Fukaya Temma <tenma.fukaya@kyash.co>
2019-05-24 13:28:33 +0900

    :ok_hand: factory_botの変更に追従してfactoryを修正した

--- spec/factories/esa_post.rb
+++ spec/factories/esa_post.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :esa_post, class: EsaArchiver::Entities::EsaPost do
     sequence :number
     category { 'path/to/post' }
-    message 'esa_archiver[skip notice]'
-    created_by 'user1'
+    message { 'esa_archiver[skip notice]' }
+    created_by { 'user1' }
   end
 end
